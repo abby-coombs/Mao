aee04ca9b748beaecfadbb5a113f39158c5f1a13
abby-coombs <87749@farnborough.ac.uk>
2019-07-01 12:10:30 +0100

    updated db manipulation

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,21 +1,19 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-
 import org.sqlite.SQLiteConfig;
 public class Main {
 
         public static Connection db = null;
 
         public static void main(String[] args) {
-            openDatabase("Users.db");
+            PlayerController.openDatabase("Main.db");
         // code to get data from, write to the database etc goes here
-            insert(0,1);
-            insert(0,2);
-            closeDatabase();
+          //  insert(0,1);
+         //   insert(0,2);
+            PlayerController.closeDatabase();
         }
 
-        private static void openDatabase(String dbFile) {
+        public static void openDatabase(String dbFile) {
             try  {
                 Class.forName("org.sqlite.JDBC");
                 //loads the database driver
@@ -23,43 +21,14 @@ public class Main {
                 config.enforceForeignKeys(true);
                 //establishes database settings (maintains structural integrity)
                 db = DriverManager.getConnection("jdbc:sqlite:resources/" + dbFile, config.toProperties());
-               //opens datavase file
+                //opens datavase file
                 System.out.println("Database connection successfully established.");
             } catch (Exception exception) {
                 //catches errors & prints message rather then closing program
                 System.out.println("Database connection error: " + exception.getMessage());
             }
 
-        }
-
-        public static void insert( int winCount, int handID) {
-            try {
-                PreparedStatement ps = db.prepareStatement("INSERT INTO Users (WinCount, HandID) VALUES (?,?)");
-                ps.setInt(1,winCount);
-                ps.setInt(2,handID);
-                ps.executeUpdate();
-                System.out.println();
-            } catch (Exception exception) {
-                System.out.println("Error: "+ exception.getMessage());
-            }
-        }
-        public static void delete(){
-            try{
-                PreparedStatement ps = db.prepareStatement("DELETE FROM Users WHERE WinCount = ?");
-                ps.setInt(1,1);
-                ps.executeUpdate();
-            }catch (Exception exception) {
-
-            }
-        }
-        public static void select(){
-
-        }
-        public static void update(){
-
-        }
-
-        private static void closeDatabase(){
+        public static void closeDatabase(){
             try {
                 db.close();
                 //closes database file
@@ -69,4 +38,4 @@ public class Main {
                 System.out.println("Database disconnection error: " + exception.getMessage());
             }
         }
-}
+}}
---
+++ src/main/java/PlayerController.java
@@ -0,0 +1,47 @@
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+public class PlayerController{
+    public static void insertPlayer(String Name, int WinCount) {
+        try {
+            PreparedStatement ps = Main.db.prepareStatement("INSERT INTO Players (Players.Name, Players.WinCount) VALUES (?,?)");
+            ps.setString(1, Name);
+            ps.setInt(2,WinCount);
+            ps.executeUpdate();
+            System.out.println();
+        } catch (Exception exception) {
+            System.out.println("Error: "+ exception.getMessage());
+        }
+    }
+    public static void deletePlayer(String Name){
+        try{
+            PreparedStatement ps = Main.db.prepareStatement("DELETE FROM Players WHERE Players.Name = ?");
+            ps.setString(1, Name);
+            ps.executeUpdate();
+        }catch (Exception exception) {
+            System.out.println("Error: "+ exception.getMessage());
+        }
+    }
+    public static void selectPlayer(){
+        try{
+            PreparedStatement ps = Main.db.prepareStatement("SELECT * FROM Players"); /*(WHERE ...)*/
+            ResultSet results = ps.executeQuery();
+            while (results.next()){
+                int playerID = results.getInt(1);
+                String playerName = results.getString(2);
+                int winCount = results.getInt(3);
+            }
+        } catch (Exception exception){
+            System.out.println("Error: "+ exception.getMessage());
+        }
+    }
+    public static void updatePlayer(String playerName, int playerID){
+        try {
+            PreparedStatement ps = Main.db.prepareStatement("UPDATE Players SET Players.Name = ? WHERE Players.PlayerID = ?");
+            ps.set
+        } catch (Exception exception) {
+
+        }
+
+    }
+}
