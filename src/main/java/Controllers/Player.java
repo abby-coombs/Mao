6ad1e4863065c9d7b9f42cc83707582bb0581a82
87749 <87749@farnborough.ac.uk>
2019-09-27 14:29:22 +0100

    fixed API stuff

--- src/main/java/Controllers/Player.java
+++ src/main/java/Controllers/Player.java
@@ -1,8 +1,13 @@
 package Controllers;
 
 import Server.Main;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
 
+import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 
@@ -28,17 +33,26 @@ public class Player {
             System.out.println("Error: "+ exception.getMessage());
         }
     }
-    public static void selectPlayer(){
+    @GET
+    @Path("list")
+    @Produces(MediaType.APPLICATION_JSON)
+    public String  selectPlayer(){
+        System.out.println("things/list");
+        JSONArray list = new JSONArray();
         try{
             PreparedStatement ps = Main.db.prepareStatement("SELECT * FROM Players"); /*(WHERE ...)*/
             ResultSet results = ps.executeQuery();
             while (results.next()){
-                int playerID = results.getInt(1);
-                String playerName = results.getString(2);
-                int winCount = results.getInt(3);
+                JSONObject item = new JSONObject();
+                item.put("id",  results.getInt(1));
+                item.put("name", results.getString(2));
+                item.put("quantity", results.getInt(3));
+                list.add(item);
             }
+            return list.toString();
         } catch (Exception exception){
-            System.out.println("Error: "+ exception.getMessage());
+            System.out.println("Database error: " + exception.getMessage());
+            return "{\"error\": \"Unable to list items, please see server console for more info.\"}";
         }
     }
     public static void updatePlayer(String playerName, int playerID){
